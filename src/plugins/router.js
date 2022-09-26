f5f7016eb1c213a02979a329a7568681110c9222
GucciGranadilla <kevidavis911@gmail.com>
2022-09-26 14:22:53 +0200

    config: add netlify deploy files

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -6,7 +6,9 @@ const routes = [
         {path: '/podcasts', name: 'podcasts', meta: { enterClass: 'animate__animated animate__fadeIn', leaveClass: 'animate__animated animate__fadeOut' }, component: () => import('@/views/Podcasts.vue')},
         {path: '/quelza', name: 'quelza', meta: { enterClass: 'animate__animated animate__fadeIn', leaveClass: 'animate__animated animate__fadeOut' }, component: () => import('@/views/Quelza.vue')},
         {path: '/live', name: 'live', meta: { enterClass: 'animate__animated animate__fadeIn', leaveClass: 'animate__animated animate__fadeOut' }, component: () => import('@/views/Live.vue')},
-]
+        {path: '/vil', name: 'vil', meta: { enterClass: 'animate__animated animate__fadeIn', leaveClass: 'animate__animated animate__fadeOut' }, component: () => import('@/views/Vil.vue')},
+
+    ]
 
 const router = createRouter({
     routes,
