8f285f5b852d0a5caf117db4b5b781d2efcebbda
GucciGranadilla <kevidavis911@gmail.com>
2022-09-27 10:27:03 +0200

    config: add netlify deploy files

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -12,14 +12,38 @@
   </template>
 
 <script>
-    import 'vue3-carousel/dist/carousel.css';
-    import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
-    export default {
-        components: {
-            Carousel,
-            Slide,
-            Pagination,
-            Navigation,
-        }
-    };
+import { defineComponent } from 'vue';
+import { Carousel, Navigation, Slide } from 'vue3-carousel';
+
+import 'vue3-carousel/dist/carousel.css';
+
+export default defineComponent({
+  name: 'Breakpoints',
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+  },
+  data: () => ({
+    // carousel settings
+    settings: {
+      itemsToShow: 1,
+      snapAlign: 'center',
+    },
+    // breakpoints are mobile first
+    // any settings not specified will fallback to the carousel settings
+    breakpoints: {
+      // 700px and up
+      700: {
+        itemsToShow: 1.5,
+        snapAlign: 'center',
+      },
+      // 1024 and up
+      1024: {
+        itemsToShow: 2.5,
+        snapAlign: 'start',
+      },
+    },
+  }),
+});
 </script>
