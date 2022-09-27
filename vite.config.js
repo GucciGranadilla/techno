f1425de23e46e6e7abc21b05f274c45820bd49c3
GucciGranadilla <kevidavis911@gmail.com>
2022-09-27 16:49:12 +0200

    config: add netlify deploy files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,10 @@ export default defineConfig({
       allow: ['..']
     }
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ registerType: 'autoUpdate' })
+    ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
