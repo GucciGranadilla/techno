4a98f89edf5a016d44cf0bfea464cbc6be320850
GucciGranadilla <kevidavis911@gmail.com>
2022-09-27 15:43:00 +0200

    config: add netlify deploy files

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@
     <button>
     <h1>Back</h1>
 </button>
-    <a><router-link :to="{name: 'home'}">Home</router-link></a>
-    <a><router-link :to="{name: 'podcasts'}">Podcasts</router-link></a>
-    <a class="menu-radio"><div class="circle"></div><router-link :to="{name: 'live'}">Live</router-link></a>
+    <router-link :to="{name: 'home'}">Home</router-link>
+    <router-link :to="{name: 'podcasts'}">Podcasts</router-link>
+    <div class="menu-radio"><div class="circle"></div><router-link :to="{name: 'live'}">Live</router-link></div>
 </div>
 </nav>
 <main class="page">
