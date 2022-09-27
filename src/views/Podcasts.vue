e90adba8adcab02bcd661b043ec3e9a5e60c2188
GucciGranadilla <kevidavis911@gmail.com>
2022-09-27 13:46:58 +0200

    config: add netlify deploy files

--- src/views/Podcasts.vue
+++ src/views/Podcasts.vue
@@ -8,25 +8,25 @@
     </transition>
   </router-view>
   <section class="hero">
-  <router-link :to="{name: 'marron'}">
+  <router-link :to="{name: 'marron'}" data-aos="fade-up" data-aos-duration="700"  data-aos-offset="500">
   <div  class="hero-img-2 mb_parallax_container" id="mb_parallax_one">
     <h1 class="hero-img-h1">MARRØN</h1>
     <h2 class="hero-img-h2">September 3rd</h2>
   </div>
   </router-link>
-  <router-link :to="{name: 'quelza'}">
+  <router-link :to="{name: 'quelza'}" data-aos="fade-up" data-aos-duration="700"  data-aos-offset="500">
   <div  class="hero-img-3 mb_parallax_container" id="mb_parallax_one">
     <h1 class="hero-img-h1">QUELZA</h1>
     <h2 class="hero-img-h2">August 2nd</h2>
   </div>
 </router-link>
-<router-link :to="{name: 'setoac'}">
+<router-link :to="{name: 'setoac'}" data-aos="fade-up" data-aos-duration="700"  data-aos-offset="500">
   <div  class="hero-img-4 mb_parallax_container" id="mb_parallax_one">
     <h1 class="hero-img-h1">SETAOC MASS</h1>
     <h2 class="hero-img-h2">March 14th</h2>
   </div>
 </router-link>
-  <router-link :to="{name: 'vil'}">
+  <router-link :to="{name: 'vil'}" data-aos="fade-up" data-aos-duration="700"  data-aos-offset="500">
   <div  class="hero-img-5 mb_parallax_container" id="mb_parallax_one">
     <h1 class="hero-img-h1">VIL</h1>
     <h2 class="hero-img-h2">February 1st</h2>
