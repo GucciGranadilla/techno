fa3f699a33ffd54f3e62afc9e2d8404bd9431011
GucciGranadilla <kevidavis911@gmail.com>
2022-09-29 10:38:26 +0200

    config: add netlify deploy files

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="content">
-<!--VideoApps code BEGIN--><div id="dplayer_flash_cae24ejwzegw" style="width:720px;height:800px;position:relative;"></div><SCRIPT LANGUAGE=JavaScript TYPE="text/javascript" SRC="//play.streamingvideoprovider.com/js/vapp.js" ></SCRIPT><SCRIPT LANGUAGE=JavaScript>var vapp = new VappController({use_div: "dplayer_flash_cae24ejwzegw", player_width: "720", player_height: "800"}, {clip_id: "5888743", player_id: "11ADAEB3I5HAF4G", playlist_id: "3667", transparent: "1", uk: "944b4cbcda1633586ed5054035c3bf65", live_id: "cae24ejwzegw", sel_playlist: "", sel_multiplaylist: "", use_html5: "true", layout: "default", theme: "light", is_responsive: "1", is_inversed: "", is_vertical: "", one_thumb_per_row: "", thumbs_size: "medium", disable_hash: "", widget_height_behavior: "0", hide_playlist: "1", hide_live_chat: "", hide_description: "1", playlist_position: "", chat_position: "", description_position: "", show_auto_play_next: "", auto_play_next: "", floating_player: "none"});</SCRIPT><!--VideoApps code END-->
-    
+      <sam-widget type="player" station-id="134720"
+        token="6a57fa3594ef9a60ee3e24f0af066d71aa328ad5"
+        anim-type='focus-in-contract' easing='ease-in-out-back' refresh-interval='30s'
+        theme-border-radius='square' image-border-radius='square' theme='{"backgroundTop":"#505050ff","backgroundBottom":"#474747","widgetBorder":"#5f5f5f","dividers":"#5f5f5f","buttons":"#bebebe","text":"#bebebe"}'>
+    </sam-widget>    
     </div>
     </template>
     
