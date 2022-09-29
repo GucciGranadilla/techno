e4fa48308881ee6adbea8845d6dc2f4ca6ae8b17
GucciGranadilla <kevidavis911@gmail.com>
2022-09-29 11:31:40 +0200

    config: add netlify deploy files

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content">
-      <sam-widget type="player" station-id="134720"
+      <sam-widget type="player" station-id="134720" class="radiostation"
         token="6a57fa3594ef9a60ee3e24f0af066d71aa328ad5"
         anim-type='focus-in-contract' easing='ease-in-out-back' refresh-interval='30s'
         theme-border-radius='square' image-border-radius='square' theme='{"backgroundTop":"#505050ff","backgroundBottom":"#474747","widgetBorder":"#5f5f5f","dividers":"#5f5f5f","buttons":"#bebebe","text":"#bebebe"}'>
